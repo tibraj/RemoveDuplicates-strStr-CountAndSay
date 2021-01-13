7544865737230d04f91b1371aeb2d1f9bcd44048
tibraj <tibraj94@gmail.com>
2021-01-13 12:45:34 -0500

    remove duplicates

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 //Remove Duplicates
-
+function removeDuplicates(numbers) {
+    if(numbers.length === 0) return 0;
+    let i = 0;
+    for (let j = 1; j < numbers.length; j++) {
+        if(numbers[j] !== numbers[i]) {
+            i++;
+            numbers[i] = numbers[j];
+        }
+    }
+    return i;
+}
 
 //strStr
 
