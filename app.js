d6f6c89dd262ef76cc65108a42b1ee158c36c1ca
tibraj <tibraj94@gmail.com>
2021-01-13 12:47:42 -0500

    strStr

--- app.js
+++ app.js
@@ -12,7 +12,16 @@ function removeDuplicates(numbers) {
 }
 
 //strStr
-
+function strStr(haystack, needle) {
+    if(needle.length === 0) return 0;
+    if(needle === haystack) return 0;
+    for(let i = 0; i < haystack.length - needle.length; i++) {
+        if(needle === haystack.substring(i, i + needle.length)) {
+            return i;
+        }
+    }
+    return -1;
+}
 
 
 //Count and Say
